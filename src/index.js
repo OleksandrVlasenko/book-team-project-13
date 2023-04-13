b0859f7ad2e3ce6b8d99eac4105cdca87676d810
ArtemBielyi <kolhozreshaet@gmail.com>
2023-04-13 23:16:04 +0300

    add toggle-color-theme

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './js/menu';
 import './js/main_page/main_page';
+import './js/toggle-color-theme';
---
+++ src/js/toggle-color-theme.js
@@ -0,0 +1,34 @@
+function addDarkClassToHTML() {
+  const theme = localStorage.getItem('theme');
+  if (theme === 'dark') {
+    document.querySelector('html').classList.add('dark');
+    document.querySelector('.switch-input').checked = true;
+  } else {
+    document.querySelector('html').classList.remove('dark');
+    document.querySelector('.switch-input').checked = false;
+  }
+}
+
+window.addEventListener('load', () => {
+  const darkModeMatchMedia = window.matchMedia('(prefers-color-scheme: dark)');
+  const isDarkMode = darkModeMatchMedia.matches ? 'dark' : 'light';
+  localStorage.theme = localStorage.theme || isDarkMode;
+  addDarkClassToHTML();
+
+  const btnEL = document.querySelector('.switch-input');
+  btnEL.addEventListener('change', event => {
+    if (event.target.checked) {
+      localStorage.setItem('theme', 'dark');
+      addDarkClassToHTML();
+    } else {
+      localStorage.setItem('theme', 'light');
+      addDarkClassToHTML();
+    }
+  });
+
+  darkModeMatchMedia.addEventListener('change', event => {
+    const newTheme = event.matches ? 'dark' : 'light';
+    localStorage.theme = newTheme;
+    addDarkClassToHTML();
+  });
+});
