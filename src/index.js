28781593e624a6ef378c1732be8452455bc29d9f
SopkoTatiana <55891393+SopkoTatiana@users.noreply.github.com>
2023-04-15 16:50:53 +0300

    block-suppUkraine

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import './js/support-ukraine';
 import './js/main_page/main_page';
 import './js/toggle-color-theme';
 import './js/modal-window-auth'
+
+// import Swiper from 'swiper';
+// import 'swiper/swiper.css';
+// import supporters from './supporters.js';
+
