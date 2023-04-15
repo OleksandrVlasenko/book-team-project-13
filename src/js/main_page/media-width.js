a8b6d28c6b9eaff178d269e4e87f7dc856f692eb
VirLg <19virlg@gmail.com>
2023-04-15 19:44:11 +0300

    add mediaQeries

---
+++ src/js/main_page/media-width.js
@@ -0,0 +1,21 @@
+
+
+
+
+export function addMediaWidth() { 
+    const checkedWidts = document.documentElement.clientWidth
+    // console.log(checkedWidts);
+    if (checkedWidts < 767) {
+        
+        return "mobile"
+    } if (checkedWidts < 1439) { 
+        return "tablet"
+    } if (checkedWidts >= 1440) { 
+        return "desctop"
+    }
+}
+
+// addMediaWidth()
+
+
+// export default {addMediaWidth }
