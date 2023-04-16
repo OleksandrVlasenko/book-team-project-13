e75f2886c38fd910c4ec1ef7815f34f9be5294bf
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-04-17 00:23:35 +0300

    added new animation

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,26 +1,26 @@
 window.addEventListener('load', () => {
-  const preloader = startPreloader();
+  startPreloader();
   stopPreloader(preloader);
 });
 
 function startPreloader() {
-  const preloader = document.querySelector('#preloader');
   document.body.classList.remove('loaded');
   document.body.classList.add('loaded_hiding');
+  // const preloader = document.querySelector('#preloader');
   return preloader;
 }
 
-function stopPreloader(preloader) {
+function stopPreloader() {
   setTimeout(() => {
     document.body.classList.add('loaded');
     document.body.classList.remove('loaded_hiding');
+    const preloader = document.querySelector('#preloader');
     preloader.remove();
-
-  }, 200);
+  }, 250);
 }
 
 function addMarkupOfPreloader() {
-  return '<div class="preloader books" id="preloader"><div class="preloader__row"><div class="preloader__item"></div><div class="preloader__item"></div></div></div>';
+  return '<div class="preloader books" id="preloader"><div class="preloader__row"><div class="preloader__item"><div></div></div></div></div>';
 }
 
 export { startPreloader, stopPreloader, addMarkupOfPreloader };
