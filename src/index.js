b8615931fb204e72b64fbcc7eebb52865a1b4375
kristinaordenans <okristin92@gmail.com>
2023-04-18 23:50:18 +0300

    scss

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './js/support-ukraine';
 import './js/main_page/main_page';
 import './js/toggle-color-theme';
 import './js/header-current-page';
-import './js/popup-about-book';
+// import './js/popup-about-book';
 import './js/refs';
 import './js/modal-auth/modal-window-authFirebase';
 import './js/modal-auth/firebaseAuth';
--- src/js/popup-about-book.js
+++ src/js/popup-about-book.js
@@ -1,19 +1,13 @@
+import { onOpenModal } from "./open-and-close-modal";
+
 const openModal = document.querySelector(`[data-modal-open]`);
 const closeModalBtn = document.querySelector(`[data-modal-close]`);
-const modal = document.querySelector(`[data-modal]`);
-const murkupConteiner = document.querySelector('.book');
+const modalWindow = document.querySelector(`[modal-window]`);
+// const modal = document.querySelector(`[data-modal]`);
+const murkupConteiner = document.querySelector('.popup-about-book__render-book-container');
 const btn = document.querySelector('#add');
 const congratMessage = document.querySelector('.under-btn-text');
 congratMessage.hidden = true;
-
-// OPEN/CLOSE MODAL
-
-// openModal.addEventListener("click", toggleModal);
-// closeModalBtn.addEventListener("click", toggleModal);
-
-function toggleModal() {
-    modal.classList.toggle("is-hidden");
-}
 
  export function modalAboutBook(e) {
     const bookEl = books.find(book => book._id === bookId);
@@ -37,24 +31,24 @@ function toggleModal() {
 
         if (book) {
             return `<div class="book">
-                   <img class="book-image" src="${book.book_image}" alt="обкладинка" width = "192px">
-                   <div class = "modal-text-content">
-                       <h2 class="book_title">${book.title}</h2>
-                       <h3 class="book_author">${book.author}</h3>
-                       <p class="book_review">${book.description}</p>
-                          <ul class = "shopping-list-container">
+                   <img class="popup__book-image" src="${book.book_image}" alt="обкладинка" width = "192px">
+                   <div class = "popup__text-content">
+                       <h2 class="popup__book_title">${book.title}</h2>
+                       <h3 class="popup__book_author">${book.author}</h3>
+                       <p class="popup__book_review">${book.description}</p>
+                          <ul class = "popup__shopping-links-container">
                              <li>
-                                <a href = "${book.buy_links[0].url}" target = "blank" noopener noreffere nofollow>
+                                <a href = "${book.buy_links[0].url}" target = "blank" noopener noreffere nofollow class="popup__shopping-links-icon">
                                    <img src = "${amazonLogo}" width = "62">
                                 </a>
                              </li>
                              <li>
-                                <a href = "${book.buy_links[1].url}" target = "blank" noopener noreffere nofollow>
+                                <a href = "${book.buy_links[1].url}" target = "blank" noopener noreffere nofollow class="popup__shopping-links-icon">
                                    <img src = "${appleBooksLogo}" width = "33">
                                 </a>
                              </li>
                              <li>
-                                <a href = "${book.buy_links[4].url}" target = "blank" noopener noreffere nofollow>
+                                <a href = "${book.buy_links[4].url}" target = "blank" noopener noreffere nofollow class="popup__shopping-links-icon">
                                    <img src = "${bookShopLogo}" width = "30">
                                 </a>
                              </li>
