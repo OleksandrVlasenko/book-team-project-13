1eb8aec5a652bd3b9587a6fe1c536061d1dad218
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-04-19 01:29:54 +0200

    retina img

--- src/js/shopping_list/imgAdress.js
+++ src/js/shopping_list/imgAdress.js
@@ -1,25 +1,77 @@
-export { bgImagesUrl, dumpImagesUrl, bookShopImagesUrl, logoAmazonImagesUrl, logoBookImagesUrl }
+export {
+    bgImagesUrlx1,
+    bgImagesUrlx2,
+    bgImagesUrlx3,
+    logoAmazonImagesUrlx1,
+    logoAmazonImagesUrlx2,
+    logoAmazonImagesUrlx3,
+    allpeBookImagesUrlx1,
+    allpeBookImagesUrlx2,
+    allpeBookImagesUrlx3,
+    bookShopImagesUrlx1,
+    bookShopImagesUrlx2,
+    bookShopImagesUrlx3,
+    dumpImagesUrl,
+}
 
-const bgImagesUrl = new URL (
+//Background images
+const bgImagesUrlx1 = new URL (
+    `../../images/shoppingList-clear--bg-x1.png`,    
+    import.meta.url
+);
+const bgImagesUrlx2 = new URL (
+    `../../images/shoppingList-clear--bg-x2.png`,    
+    import.meta.url
+);
+const bgImagesUrlx3 = new URL (
     `../../images/shoppingList-clear--bg-x3.png`,    
     import.meta.url
 );
 
+// Amazon
+const logoAmazonImagesUrlx1 = new URL(
+    `../../images/shoppingList--logoAmazon--x1.png`,
+    import.meta.url
+);
+const logoAmazonImagesUrlx2 = new URL(
+    `../../images/shoppingList--logoAmazon--x2.png`,
+    import.meta.url
+);
+const logoAmazonImagesUrlx3 = new URL(
+    `../../images/shoppingList--logoAmazon--x3.png`,
+    import.meta.url
+);
+
+// Apple Book
+const allpeBookImagesUrlx1 = new URL(
+    `../../images/shoppingList--appleBook--x1.png`,
+    import.meta.url
+);
+const allpeBookImagesUrlx2 = new URL(
+    `../../images/shoppingList--appleBook--x2.png`,
+    import.meta.url
+);
+const allpeBookImagesUrlx3 = new URL(
+    `../../images/shoppingList--appleBook--x3.png`,
+    import.meta.url
+);
+
+// Boock Shop
+const bookShopImagesUrlx1 = new URL(
+    `../../images/shoppingList--bookShop--x1.png`,
+    import.meta.url
+);
+const bookShopImagesUrlx2 = new URL(
+    `../../images/shoppingList--bookShop--x2.png`,
+    import.meta.url
+);
+const bookShopImagesUrlx3 = new URL(
+    `../../images/shoppingList--bookShop--x3.png`,
+    import.meta.url
+);
+
+// Remove book button
 const dumpImagesUrl = new URL (
     `../../images/dump.svg`,    
-    import.meta.url
-);
-
-const bookShopImagesUrl = new URL(
-    `../../images/shoppingList--bookShop.png`,
-    import.meta.url
-);
-
-const logoBookImagesUrl = new URL(
-    `../../images/shoppingList--logoBook.png`,
-    import.meta.url
-);
-const logoAmazonImagesUrl = new URL(
-    `../../images/shoppingList--logoAmazon.png`,
     import.meta.url
 );
