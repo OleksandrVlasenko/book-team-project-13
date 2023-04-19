69bd5d2fba5d210ea940ebacdb0631058097fc80
ArtemBielyi <kolhozreshaet@gmail.com>
2023-04-20 00:03:34 +0300

    ....

--- src/js/header-current-page.js
+++ src/js/header-current-page.js
@@ -22,7 +22,7 @@ console.log(currentPath);
 if (
   currentPath === '/book-team-project-13/index.html' ||
   currentPath === '/index.html' ||
-  currentPath === 'book-team-project-13'
+  currentPath === '/book-team-project-13'
 ) {
   // console.log(currentPath);
   homePg.classList.add('current__page');
